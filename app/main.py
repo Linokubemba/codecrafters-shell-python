dcd2159219e69bcf2a3088e4eef6e57bcdcb1fdc
linokubemba <emmanuel.lino-kubemba@outlook.fr>
2025-02-17 23:22:19 +0100

    type builtin implementation second attempt

--- app/main.py
+++ app/main.py
@@ -1,26 +1,39 @@
 import sys
 
+def echo(args: str):
+    print(args)
+
+def exit(status: int):
+    sys.exit(status)
+
+def type(cmd: str):
+    suffix = ": command not found"
+    if cmd in COMMANDS:
+        suffix = " is a shell builtin"
+    print(cmd + suffix)
+
 def parse_cmd(input: str) -> str | None:
-    cmd, *args = input.split(" ", 1)    
+    cmd, *args = input.split(" ", 1)
     return cmd, args
 
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
+        
         cmd, args = parse_cmd(input())
-        match cmd:
-            case "exit":
-                sys.exit(args[0])
-            case "echo":
-                print(args[0])
-            case "type" | _:
-                if cmd == "type" and args[0] in ["type", "echo", "exit"]:
-                    print(args[0], "is a shell builtin")
-                else:
-                    print(cmd + ": command not found")
+        try:
+            run = COMMANDS[cmd]
+            run(args[0])
+        except Exception:
+            print(cmd + ": command not found")
 
 
 
 if __name__ == "__main__":
+    COMMANDS = {
+        "echo": echo,
+        "exit": exit,
+        "type": type
+    }
     main()

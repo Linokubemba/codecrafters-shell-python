fdeefb37ce9e1b53e0a984cbd1406b36723b9001
linokubemba <emmanuel.lino-kubemba@outlook.fr>
2025-02-23 22:19:43 +0100

    run a program first try

--- app/main.py
+++ app/main.py
@@ -1,28 +1,25 @@
 import sys
 import subprocess
 
-def run(cmd, args):
-    match cmd:
-        case "echo":
-            print(args)
-        case "type":
-            status, output = subprocess.getstatusoutput(f"type {args}")
-            if status == 0:
-                print(output)
-            else:
-                raise ValueError(args + ": not found")
-        case default:
-            raise ValueError(cmd + ": command not found")
+
+def exec_sh(input):
+    if input == "exit 0":
+        sys.exit(0)
+
+    status, output = subprocess.getstatusoutput(input)
+    cmd, *args = input.split(" ", 1)
+    if status != 0:
+        msg = f"{args[0]}:" if args else f"{cmd}: command"
+        raise ValueError(f"{msg} not found")
+
+    print(output)
+
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        cmd, *args = input().split(" ", 1)
-        if cmd == "exit":
-            sys.exit(0)
-
         try:
-            run(cmd, args[0] if args else "")
+            exec_sh(input())
         except Exception as e:
             print(e)
 

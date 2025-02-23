ff539ec69306732bc8a2ab5d9adba61df89c84e5
linokubemba <emmanuel.lino-kubemba@outlook.fr>
2025-02-23 23:35:01 +0100

    pwd first try

--- app/main.py
+++ app/main.py
@@ -1,25 +1,46 @@
+import os
 import sys
 import subprocess
 
 
-def exec_sh(input):
-    if input == "exit 0":
-        sys.exit(0)
+BUILTIN_CMDS = {
+    "echo", "type", "exit", "pwd"
+}
 
+def exec_builtin(cmd, args):
+    match cmd:
+        case "pwd":
+            return os.getcwd()
+        case "echo":
+            return args
+        case "type":
+            if args in BUILTIN_CMDS:
+                return args + " is a shell builtin"
+            return exec_sh(cmd, args)
+
+def exec_sh(cmd, args):
+    input = " ".join([cmd, args])
     status, output = subprocess.getstatusoutput(input)
-    cmd, *args = input.split(" ", 1)
     if status != 0:
-        msg = f"{args[0]}:" if args else f"{cmd}: command"
+        msg = f"{args}:" if args else f"{cmd}: command"
         raise ValueError(f"{msg} not found")
-
-    print(output)
-
+    return output
 
 def main():
     while True:
         sys.stdout.write("$ ")
+        sh_input = input()        
         try:
-            exec_sh(input())
+            if sh_input == "exit 0":
+                sys.exit(0)
+
+            cmd, *args = sh_input.split(" ", 1)
+            args = args[0] if args else ""
+            output = exec_builtin(cmd, args) \
+                    if cmd in BUILTIN_CMDS \
+                    else exec_sh(cmd, args)
+
+            sys.stdout.write(output + "\n")
         except Exception as e:
             print(e)
 

24640484fe267a74d857dc537673e05fdf6397d7
linokubemba <emmanuel.lino-kubemba@outlook.fr>
2025-02-24 20:43:31 +0100

    cd implementation

--- app/main.py
+++ app/main.py
@@ -3,46 +3,71 @@ import sys
 import subprocess
 
 
-BUILTIN_CMDS = {
-    "echo", "type", "exit", "pwd"
-}
+class BuiltinHandler:
+    def __init__(self):
+        self.BUILTIN_CMDS = {
+            "echo", "type", "exit", "pwd", "cd"
+        }
+        self.active_dir = os.getcwd()
+    
+    def echo(self, args):
+        print(args)
+    
+    def exit(self, args):
+        sys.exit(int(args))
 
-def exec_builtin(cmd, args):
-    match cmd:
-        case "pwd":
-            return os.getcwd()
-        case "echo":
-            return args
-        case "type":
-            if args in BUILTIN_CMDS:
-                return args + " is a shell builtin"
-            return exec_sh(cmd, args)
+    def pwd(self, _):
+        print(self.active_dir)
+    
+    def cd(self, args):
+        path_dirs = self.active_dir.split("/")
+        target_dirs = args.split("/") if "/" in args else [args]
+        for i, dir in enumerate(target_dirs):
+            if dir == "" and i == 0:
+                path_dirs = []
+            elif dir == "..":
+                path_dirs.pop()
+                continue
+            path_dirs.append(dir if dir != "." else "")
+        
+        tmp = "/".join(path_dirs)
+        if not os.path.exists(tmp):
+            raise ValueError("cd: " + tmp + ": No such file or directory")
+        self.active_dir = tmp
 
-def exec_sh(cmd, args):
-    input = " ".join([cmd, args])
-    status, output = subprocess.getstatusoutput(input)
-    if status != 0:
-        msg = f"{args}:" if args else f"{cmd}: command"
-        raise ValueError(f"{msg} not found")
-    return output
+    def type(self, args):
+        if not (args in self.BUILTIN_CMDS):
+            for path in os.environ.get("PATH").split(os.pathsep):
+                cmd_path = os.path.join(path, args)
+                if os.path.exists(cmd_path):
+                    print(" ".join([args, "is", cmd_path]))
+                    return
+            
+            raise ValueError(args + ": not found")
+        print(args + " is a shell builtin")
+
 
 def main():
+    builtin_executor = BuiltinHandler()
     while True:
         sys.stdout.write("$ ")
-        sh_input = input()        
-        try:
-            if sh_input == "exit 0":
-                sys.exit(0)
+        sys.stdout.flush()
+        sh_input = input()
 
+        try:
             cmd, *args = sh_input.split(" ", 1)
             args = args[0] if args else ""
-            output = exec_builtin(cmd, args) \
-                    if cmd in BUILTIN_CMDS \
-                    else exec_sh(cmd, args)
+            if cmd in builtin_executor.BUILTIN_CMDS:
+                run = getattr(builtin_executor, cmd)
+                run(args)
+            else:
+                status, output = subprocess.getstatusoutput(sh_input)
+                if status != 0:
+                    raise ValueError(cmd + ": command not found")
+                sys.stdout.write(output + "\n")
 
-            sys.stdout.write(output + "\n")
         except Exception as e:
-            print(e)
+            sys.stdout.write(e.__str__() + "\n")
 
 
 if __name__ == "__main__":
